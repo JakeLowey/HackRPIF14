6f85aa66d41949df12172e26b66a6ab8fde3c833
PeterRyder1 <peter.w.ryder@gmail.com>
2014-11-15 17:16:54 -0500

    attempt to render html page

--- HackRPI/HackRPI/urls.py
+++ HackRPI/HackRPI/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^/templates/HackRPI/', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', 'HackRPI.views.index', name='index'),
 )
