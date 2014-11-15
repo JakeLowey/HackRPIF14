a2e759fa42f86cd22e288bf7bf45d0f40d141f0b
PeterRyder1 <peter.w.ryder@gmail.com>
2014-11-15 16:50:46 -0500

    Linked with azure database

--- HackRPI/HackRPI/settings.py
+++ HackRPI/HackRPI/settings.py
@@ -59,8 +59,9 @@ WSGI_APPLICATION = 'HackRPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'HackRPI',
+        'HOST': 'o99jonehn9.database.windows.net'
     }
 }
 
